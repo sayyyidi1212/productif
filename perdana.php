bf9dd79cc0402d7003c3e6a71c7cc7751fe7334c
sayyidi1212 <saydand7@gmail.com>
2023-08-30 21:55:31 +0700

    first upload

---
+++ perdana.php
@@ -0,0 +1,17 @@
+
+
+<?php $nilai = 2022; 
+if($nilai> 2023)
+{
+    echo "makanan tidak layak di jual";
+}
+elseif($nilai> 2019)
+{
+    echo "makanan layak jual";
+}
+else
+{
+    echo "wewewewewe";
+}
+?>
+
